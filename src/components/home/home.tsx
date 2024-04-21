13fe7e7f706bce6fa9fba73e1f6e9e9847c3f947
ScriptSorcery <155194281+ScriptSorcery@users.noreply.github.com>
2024-04-21 16:43:07 +0000

    Clean up code

---
+++ src/components/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+interface Props {
+
+}
+
+const home = (props: Props) => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+interface Props {
+    
+}
+
+const navBar = (props:Props) => {
+  return (
+    <div>navBar</div>
+  )
+}
+
+export default navBar
